4ceffcda8395909b515ca86d6f6eff978d32b9d6
Max Young <max.young@lcs.com>
2017-10-08 11:51:34 -0400

    Add project files.

---
+++ Mulligan.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mulligan.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mulligan.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8a17e618-6e74-4fed-9b5d-0a8dec3fec0c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Mulligan/Retry.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Threading;
+
+namespace Mulligan
+{
+    public class Retry
+    {
+        public static readonly TimeSpan DefaultRetryFor = TimeSpan.FromMilliseconds(1000);
+        private static readonly TimeSpan DefaultRetryInterval = TimeSpan.FromMilliseconds(200);
+
+        /// <summary>
+        /// Retries a action until the action succeeds or until timeout is reached.
+        /// </summary>
+        /// <param name="action">Action that will be retried</param>
+        /// <param name="timeout">Time the action will be retried</param>
+        /// <param name="retryInterval">Interval between retries</param>
+        public static void While(Action action, TimeSpan timeout, TimeSpan? retryInterval = null)
+        {
+            DateTime startTime = DateTime.Now;
+
+            while (true)
+            {
+                try
+                {
+                    action();
+                    return;
+                }
+                catch (Exception exception)
+                {
+                    if (IsTimedOut(startTime, timeout))
+                        throw new TimeoutException("Timeout occured while retrying", exception);
+
+                    Thread.Sleep(retryInterval ?? DefaultRetryInterval);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retries a function until the function succeeds or until timeout is reached.
+        /// </summary>
+        /// <typeparam name="T">Return type of the function</typeparam>
+        /// <param name="function">Function that will be retried</param>
+        /// <param name="timeout">Time the action will be retried</param>
+        /// <param name="retryInterval">Interval between retries</param>
+        /// <returns>Return of the function</returns>
+        public static T While<T>(Func<T> function, TimeSpan timeout, TimeSpan? retryInterval = null)
+        {
+            DateTime startTime = DateTime.Now;
+
+            while (true)
+            {
+                try
+                {
+                    return function();
+                }
+                catch (Exception exception)
+                {
+                    Thread.Sleep(retryInterval ?? DefaultRetryInterval);
+
+                    if (IsTimedOut(startTime, timeout))
+                        throw new TimeoutException("Timeout occured while retrying", exception);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retries a function until the predicate evaluates false and the function succeeds or until timeout is reached.
+        /// </summary>
+        /// <typeparam name="T">Return type of the function</typeparam>
+        /// <param name="predicate">Predicate that evaluates the results of the function</param>
+        /// <param name="function">Function that will be retried</param>
+        /// <param name="timeout">Time the action will be retried</param>
+        /// <param name="retryInterval">Interval between retries</param>
+        /// <returns>Return of the function</returns>
+        public static T While<T>(Predicate<T> predicate, Func<T> function, TimeSpan timeout, TimeSpan? retryInterval = null)
+        {
+            DateTime startTime = DateTime.Now;
+
+            while (true)
+            {
+                try
+                {
+                    T result = function();
+                    if (!predicate(result))
+                        return result;
+
+                    if (IsTimedOut(startTime, timeout))
+                        return result;
+                }
+                catch (Exception exception)
+                {
+                    Thread.Sleep(retryInterval ?? DefaultRetryInterval);
+
+                    if (IsTimedOut(startTime, timeout))
+                        throw new TimeoutException("Timeout occured while retrying", exception);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retries a function until the predicate evaluates false and the function succeeds or until timeout is reached.
+        /// </summary>
+        /// <typeparam name="T">Return type of the function</typeparam>
+        /// <param name="predicate">Predicate that evaluates the results of the function</param>
+        /// <param name="function">Function that will be retried</param>
+        /// <param name="tryCatchHandler">Custom exception handling for function</param>
+        /// <param name="timeout">Time the action will be retried</param>
+        /// <param name="retryInterval">Interval between retries</param>
+        /// <returns>Return of the function</returns>
+        public static T While<T>(Predicate<T> predicate, Func<T> function, Func<Func<T>, T> tryCatchHandler, TimeSpan timeout, TimeSpan? retryInterval = null)
+        {
+            DateTime startTime = DateTime.Now;
+
+            while (true)
+            {
+                T result = tryCatchHandler(function);
+                if (!predicate(result))
+                    return result;
+
+                if (IsTimedOut(startTime, timeout))
+                    return result;
+            }
+        }
+
+        private static bool IsTimedOut(DateTime startTime, TimeSpan timeout)
+        {
+            // Check for infinite timeout
+            if (timeout.TotalMilliseconds < 0)
+            {
+                return false;
+            }
+            return DateTime.Now.Subtract(startTime) >= timeout;
+        }
+    }
+}
